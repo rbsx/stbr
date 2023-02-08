2e4fcc670b2f1cdd6071616f466c24f4f317c525
ignat.rubis <ignat.rubis@team.bumble.com>
2023-02-08 15:30:14 +0000

    Updates

--- src/components/modal-overlay/modal-overlay.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./modal-overlay.module.css";
-
-export const ModalOverlay = ({ onClick }: { onClick: () => void }) => {
-  return <div className={styles.overlay} onClick={onClick} data-cy="modal-overlay"/>;
-};
--- src/pages/not-fount-404/not-fount-404.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound404 = () => {
-  return (
-    <h3 className={`pb-6 text text_type_main-large`}>
-      Страница не найдена. Ошибка 404.
-    </h3>
-  );
-};
-
-export default NotFound404;
